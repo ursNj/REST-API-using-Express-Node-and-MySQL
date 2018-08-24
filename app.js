791e3045b6636d2f0415c19f21256471e8903787
Niranjan Reddy Devasani <28749151+ursNj@users.noreply.github.com>
2018-08-24 08:19:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// Require packages and set the port
+const express = require('express');
+const port = 3002;
+const bodyParser = require('body-parser');
+const app = express();
+
+const routes = require('./routes/routes');
+
+// Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+ 
+// app.get('/', (request, response) => {
+//     response.send({
+//         message: 'Node.js and Express REST API'}
+//     );
+// });
+ 
+routes(app);
+
+// Start the server
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+ 
+    console.log(`Server listening on port ${server.address().port}`);
+});
---
+++ src/routes/routes.js
@@ -0,0 +1,29 @@
+const users = [{
+    id: 1,
+    name: "Richard Hendricks",
+    email: "richard@piedpiper.com",
+},
+{
+    id: 2,
+    name: "Bertram Gilfoyle",
+    email: "gilfoyle@piedpiper.com",
+},
+];
+
+const router = app => {
+
+    app.get('/', (request, response) => {
+        response.send({
+            message: 'Node.js and Express REST API'
+        });
+    });
+
+    app.get('/users', (request, response) => {
+        response.send(users);
+    });
+    
+}
+
+
+
+module.exports = router;
